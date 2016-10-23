017e3e75398a1a54d988ab84ea0de7e49ff9f20b
Sabel <draisy.sabel@eikospartners.com>
2016-10-23 01:14:42 -0400

    update timepicker to handle formatting

--- src/timepicker.js
+++ src/timepicker.js
@@ -1,30 +1,32 @@
 import ko from 'knockout';
 import $ from 'jquery';
+import moment from 'moment';
 import 'timepicker';
 import 'timepicker/jquery.timepicker.min.css';
 
-    ko.bindingHandlers.timepicker = {
-        init: function (element, valueAccessor) {
-            var options = valueAccessor();
-            var data = options.data;
-            var picker = $(element).timepicker(options);
+ko.bindingHandlers.timepicker = {
+    init: function (element, valueAccessor) {
+        var options = valueAccessor();
+        var data = options.data;
+        var format = options.format || '';
+        var picker = $(element).timepicker(options);
 
-            if(ko.isObservable(data)) {
-              //set initial values
-              if(data()) {
-                  $(element).timepicker('setTime', data());
-              } else {
-                  data($(element).timepicker('getTime'));
-              }
+        if (ko.isObservable(data)) {
+            //set initial values
+            if (data()) {
+                $(element).timepicker('setTime', moment(data(), format).toDate());
+            } else {
+                data(moment($(element).timepicker('getTime')).format(format));
+            }
 
-              picker.on('change', function() {
-                data($(element).timepicker('getTime'));
-              });
-              data.subscribe(function (newTime) {
-                  $(element).timepicker('setTime', newTime);
-              })
-          } else if(data) {
-              $(element).timepicker('setTime', data);
-          }
+            picker.on('change', function () {
+                data(moment($(element).timepicker('getTime')).format(format));
+            });
+            data.subscribe(function (newTime) {
+                $(element).timepicker('setTime', moment(newTime, format).toDate());
+            })
+        } else if (data) {
+            $(element).timepicker('setTime', moment(data, format).toDate());
         }
-    };
+    }
+};
