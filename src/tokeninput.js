4eb71bc1be0deea4c61e39bf42340d55062de8c0
Gucciardo <ADCWTMV@ams.bnymellon.net>
2017-01-24 15:20:52 -0500

    fix token input destroy

--- src/tokeninput.js
+++ src/tokeninput.js
@@ -5,105 +5,104 @@ import _ from 'lodash';
 import 'jquery-tokeninput';
 import 'jquery-tokeninput/styles/token-input.css';
 
-    ko.bindingHandlers.tokeninputSource = {
-        init: function(
-            element,
-            valueAccessor,
-            allBindings
-        ) {
-            var params = valueAccessor(),
-                bindings = allBindings(),
-                $selectedElement = $(element),
-                value = bindings.tokeninputValue || ko.observable(),
-                tokeninputUpdating = false,
+ko.bindingHandlers.tokeninputSource = {
+    init: function (
+        element,
+        valueAccessor,
+        allBindings
+    ) {
+        const params = valueAccessor(),
+            bindings = allBindings(),
+            $selectedElement = $(element),
+            value = bindings.tokeninputValue || ko.observable();
+        let tokeninputUpdating = false,
+            reinit = false;
+
+        function getOptions() {
+            return merge({
+                theme: 'pjson',
+                onResult: onResult,
+                onAdd: onAdd,
+                onDelete: onDelete,
+                preventDuplicates: true,
+                minChars: 0,
+                caching: false,
+                searchDelay: 0,
+                animateDropdown: false,
+                allowTabOut: true,
+                noResultsText: null,
+                searchingText: null,
+                disabled: ko.unwrap(bindings.tokeninputDisable),
+                prePopulate: params.filter(result => (value() || []).includes(result.id))
+            }, bindings.tokenInputOptions);
+        }
+
+        function onResult(results) {
+            // console.log('Results -->', results);
+            return results.filter(result => !value().includes(result.id));
+        }
+
+        function onAdd(added) {
+            // console.log('Add -->', added);
+            tokeninputUpdating = true;
+            value.push(added.id);
+            tokeninputUpdating = false;
+        }
+
+        function onDelete(deleted) {
+            // console.log('Delete -->', deleted);
+            if (reinit) {
+                return;
+            }
+            tokeninputUpdating = true;
+            value.remove(deleted.id);
+            tokeninputUpdating = false;
+        }
+
+        function init() {
+            $selectedElement.tokenInput(params, getOptions());
+        }
+
+        init();
+
+        value.subscribe(() => {
+            if (!tokeninputUpdating) {
+                reinit = true;
+                $selectedElement.tokenInput('destroy');
+                init();
                 reinit = false;
-            
-            function getOptions() {
-                return merge({ 
-                    theme: 'pjson', 
-                    onResult: onResult,
-                    onAdd: onAdd,
-                    onDelete: onDelete,
-                    preventDuplicates: true,
-                    minChars: 0,
-                    caching: false,
-                    searchDelay: 0,
-                    animateDropdown: false,
-                    allowTabOut: true,
-                    noResultsText: null,
-                    searchingText: null,
-                    disabled: ko.unwrap(bindings.tokeninputDisable),
-                    prePopulate: params.filter(function (result) {
-                        return (value() || []).includes(result.id);
-                    }),
-                }, bindings.tokenInputOptions)
             }
-            
-            function onResult(results) {
-                //console.log('Results -->', results);
-                return results.filter(function (result) {
-                    return !value().includes(result.id);
-                });
-            }
+        });
 
-            function onAdd(added) {
-                //console.log('Add -->', added);
-                tokeninputUpdating = true;
-                value.push(added.id);
-                tokeninputUpdating = false;
-            }
+        // if (options.placeholder) {
+        //     $selectedElement.parent().find('input').attr('placeholder', options.placeholder);
+        // }
 
-            function onDelete(deleted) {
-                //console.log('Delete -->', deleted);
-                if (reinit) {
-                    return;
-                }
-                tokeninputUpdating = true;
-                value.remove(deleted.id);
-                tokeninputUpdating = false;
-            }
-
-            function init() {
-                $selectedElement.tokenInput(params, getOptions());
-            }
-
-            init();
-
-            value.subscribe(function() {
-                if(!tokeninputUpdating) {
-                    reinit = true;
-                    $selectedElement.tokenInput('destroy');
-                    init();
-                    reinit = false;
-                }
-            })
-            
-            // if (options.placeholder) {
-            //     $selectedElement.parent().find('input').attr('placeholder', options.placeholder);
-            // }
-        },
-        update: function(
-            element,
-            valueAccessor,
-            allBindings
-        ) {
-            var params = valueAccessor();
-            //console.log('new params-->', params);
-            $(element).data('settings').local_data = params;
-        }
+        ko.utils.domNodeDisposal.addDisposeCallback(element, () => {
+            $selectedElement.tokenInput('destroy');
+        });
+    },
+    update: function (
+        element,
+        valueAccessor
+    ) {
+        const params = valueAccessor();
+        // console.log('new params-->', params);
+        $(element).data('settings').local_data = params;
     }
-    
-    ko.bindingHandlers.tokeninputDisable = {
-        update: function (
-            element,
-            valueAccessor
-        ) {
-            let isDisabled = valueAccessor();
+};
 
-            $(element).tokenInput('toggleDisabled', ko.unwrap(isDisabled));
-        }
-    };
+ko.bindingHandlers.tokeninputDisable = {
+    update: function (
+        element,
+        valueAccessor
+    ) {
+        const isDisabled = valueAccessor();
 
-    ko.bindingHandlers.tokeninputOptions = {};
+        $(element).tokenInput('toggleDisabled', ko.unwrap(isDisabled));
+    }
+};
 
-    ko.bindingHandlers.tokeninputValue = {};
+ko.bindingHandlers.tokeninputOptions = {};
+
+ko.bindingHandlers.tokeninputValue = {};
