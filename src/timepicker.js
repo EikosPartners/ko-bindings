686150ca72d290264e18129c0cd4b8e6269cf51f
Sabel <draisy.sabel@bnymellon.com>
2017-01-26 17:19:46 -0500

    update timepicker for utc dates

--- src/timepicker.js
+++ src/timepicker.js
@@ -6,25 +6,32 @@ import 'timepicker/jquery.timepicker.min.css';
 
 ko.bindingHandlers.timepicker = {
     init: function (element, valueAccessor) {
-        var options = valueAccessor();
-        var data = options.data;
-        var format = options.format || '';
-        var picker = $(element).timepicker(options);
-        var time;
+        const options = valueAccessor();
+        const data = options.data;
+        const format = options.format || '';
+        const picker = $(element).timepicker(options);
+        let m = moment,
+            time;
+
+
+        if (options.utc) {
+            m = m.utc;
+        }
 
         if (ko.isObservable(data)) {
             //set initial values
             if (data()) {
-                $(element).timepicker('setTime', moment(data(), format).toDate());
+                $(element).timepicker('setTime', m(data(), format).toDate());
             } else {
                 time = $(element).timepicker('getTime'); // Does this ever return a value?
                 if (time) {
-                    data(moment(time).format(format));
+                    data(m(time).format(format));
                 }
             }
 
             picker.on('change', function () {
-                let newValue = moment($(element).timepicker('getTime'));
+                let newValue = m($(element).timepicker('getTime'));
+            
                 if(newValue.isValid()) {
                     data(newValue.format(format));
                 } else {
@@ -32,10 +39,10 @@ ko.bindingHandlers.timepicker = {
                 }
             });
             data.subscribe(function (newTime) {
-                $(element).timepicker('setTime', moment(newTime, format).toDate());
+                $(element).timepicker('setTime', m(newTime, format).toDate());
             });
         } else if (data) {
-            $(element).timepicker('setTime', moment(data, format).toDate());
+            $(element).timepicker('setTime', m(data, format).toDate());
         }
 
         picker.on('showTimepicker', function() {
